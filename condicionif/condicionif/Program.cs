394fda6f3809bcbd2379e4004fec5e2f8249abc9
claudioalexist <claudiotapia.ctb@gmail.com>
2022-07-18 15:28:28 -0400

    condicionif
    MIENTRAS num sea menor a 10.

---
+++ condicionif/condicionif/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace condicionif
+{
+    class MainClass
+    { 
+
+    public static void Main(string[] args)
+        {
+            int num = 0;
+
+
+            while (num < 10) // MIENTRAS num sea menor a 10.
+            {
+
+               Console.WriteLine("hola mundo"); // muestrame Hola mundo.
+                num++; //incrementa 1 cada ves que el bucle se ejecuta.                       
+
+            }
+                Console.ReadKey();
+        }
+    }
+}
