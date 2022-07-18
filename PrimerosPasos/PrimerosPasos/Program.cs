ab5a14f2e81b0f3342644a55fef910626cdeecea
claudioalexist <claudiotapia.ctb@gmail.com>
2022-07-18 16:58:25 -0400

    Primeros Pasos
    con if y else

---
+++ PrimerosPasos/PrimerosPasos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PrimerosPasos
+{
+   public class PrimeraClase
+    {
+        public static void Main(string[] args)
+        {
+            int edad;
+            string linea;
+
+            Console.Write("Introduce tu edad por favor:");
+
+            linea = Console.ReadLine();
+            edad = int.Parse(linea);
+
+            if (edad < 18)
+            {
+                Console.WriteLine("Eres un adolecente");
+            }
+            else if (edad < 40)
+            {
+                Console.WriteLine("Eres Joven");
+            }
+            else if (edad < 65)
+            {
+                Console.WriteLine("Eres maduro");
+            }
+            else
+            {
+                Console.WriteLine("Cuidate");
+            }
+
+            Console.ReadKey();
+        } 
+    } 
+}
---
+++ PrimerosPasos/PrimerosPasos/Variables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace PrimerosPasos
+{
+    public class Variables
+    {
+        public static void main(string[] args)
+        {
+            int a = 5;
+            int b = 7;
+
+            int c = b + a;
+
+
+            Console.Write(c);
+        }
+    }
+}
