6e2f0c66ea6e91306457cc6447484eb8a49c36f1
claudioalexist <claudiotapia.ctb@gmail.com>
2022-07-18 15:05:56 -0400

    Array
    prueba Array

---
+++ Array/Array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+
+            a[0] = 7;
+            a[1] = 8;
+            a[2] = 3;
+            //a[3] = 4;
+            //a[4] = 5;
+
+            foreach (int x in a)
+            {
+                Console.WriteLine(x);
+            }
+
+
+
+        }
+    }
+}
