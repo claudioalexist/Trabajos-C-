3d27aa6036fb18ef80e3f9b18118df7014ce2bbf
claudioalexist <claudiotapia.ctb@gmail.com>
2022-07-18 15:31:46 -0400

    dowhile-Haz mientras

---
+++ dowhile/dowhile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace dowhile
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //DO-WHILE -> HAZ-MIENTRAS
+
+            int num = 0;
+
+            do
+            {
+                Console.WriteLine("Hola mundo con do while");
+                num++;
+            }
+            while (num < 10); 
+
+        }
+    }
+}
