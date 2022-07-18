770dab467d9cac084268b4034690eb59e6f8c6e6
claudioalexist <claudiotapia.ctb@gmail.com>
2022-07-18 15:29:35 -0400

    condicion if else

---
+++ condicionyy/condicionyy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace condicionyy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            string linea;
+
+            Console.Write("Ingresa primer numero:");
+
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+
+            Console.Write("Ingresa segundo numero:");
+
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+
+            Console.Write("Ingresa tercer número:");
+
+            linea = Console.ReadLine();
+            num3 = int.Parse(linea);
+
+            if (num1 == num2 || num1 == num3) // SI num1 es mayor a num2 Y! si num1 es mayor a num3...
+            {
+                Console.WriteLine(num1); // Muestrame num1
+            }
+            {
+                if (num2 > num3) // si num2 es mayor a num3...
+
+                    Console.WriteLine(num3); // muestrame num3.
+
+                else
+                {
+                    Console.WriteLine(num2); //En lo contrario muestrame num2.
+                }
+
+            }
+
+            Console.ReadKey(); //fin del programa. 
+        }
+    }
+}
