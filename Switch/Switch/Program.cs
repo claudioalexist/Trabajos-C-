aa454474e8b7c6255ca25f20ccca3fc6b9c5d9e9
claudioalexist <claudiotapia.ctb@gmail.com>
2022-07-18 17:11:11 -0400

    Switch

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Switch
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i=0; i<5; i++)
+            {
+                Console.WriteLine("Elija un valor del 1 al 5:");
+
+                int valor = int.Parse(Console.ReadLine());
+
+                switch (valor)
+                {
+                    case 1:
+                        Console.WriteLine("uno");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("dos");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("tres");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("cuatro");
+                        break;
+                    case 5:
+                        Console.WriteLine("cinco");
+                        break;
+
+                    default:
+                        Console.WriteLine("Numero fuera de rango.");
+                        break;
+                }
+
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
