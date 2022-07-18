90db990903564224d91fcc5b42088700951d2202
claudioalexist <claudiotapia.ctb@gmail.com>
2022-07-18 17:01:06 -0400

    Problema While

---
+++ problemaWhile/problemaWhile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace problemaWhile
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = 1; // Contador "X" que nos sirve para contar las veltas que debe repetir el while.
+            int n; // Varieble "N" para guardar el dato ingresado desde consola.
+            int suma = 0;
+            //int prom; 
+            string linea;
+
+            Console.Write("Ingrese un valor:"); 
+
+            linea = Console.ReadLine();
+            n = int.Parse(linea);
+            suma = n + n;
+
+
+            while (x <= n) // Mientras (While) que "X" sea menos o igual a "N"...
+            {
+                Console.Write(suma); // Muestrame en valor de "X".
+                Console.Write(" - ");
+                x = x + 1; // "X" ve sumandomelo cada 4.
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
