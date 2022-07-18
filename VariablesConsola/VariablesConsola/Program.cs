27a42d47178eda4d951e1ffa6f991ed9c1e1570a
claudioalexist <claudiotapia.ctb@gmail.com>
2022-07-18 17:15:50 -0400

    VariablesConsola

---
+++ VariablesConsola/VariablesConsola/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VariablesConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entero = 5;
+            string cadena = "c#";
+            bool valor = false;
+
+            Console.WriteLine("La variable de tipo entero contiene:  {0}, La variable de tipo cadena contiente: {1}, la variable de tipo bool contiente:  {2}",entero,cadena,valor);
+            Console.ReadKey();
+        }
+    }
+}
