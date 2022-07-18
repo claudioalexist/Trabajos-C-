ad5605b19336710ce4e57a1c7db9df4e40d3d36e
claudioalexist <claudiotapia.ctb@gmail.com>
2022-07-18 17:16:46 -0400

    WhileContrasena

---
+++ WhileContrasena/WhileContrasena/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WhileContrasena
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int correcto = 0;
+            string usuario;
+            string pass;
+
+
+            while (correcto == 0)
+            {
+               //Console.Clear();
+                Console.WriteLine("Escriba su nombre de usuario");
+                usuario = Console.ReadLine();
+
+                Console.WriteLine("Escriba su contraseña");
+                pass = Console.ReadLine();
+
+                if (usuario == "usuario correcto" && pass == "password correcto")
+                {
+                    correcto = 1;
+                   
+                }
+            }
+
+            Console.ReadKey();
+        }
+        }
+        
+        }
+
