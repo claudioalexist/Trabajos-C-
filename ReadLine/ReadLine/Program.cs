e9d5a426e28e2969f77172f0277c5ad1603da4e0
claudioalexist <claudiotapia.ctb@gmail.com>
2022-07-18 17:05:57 -0400

    prueba con readline

---
+++ ReadLine/ReadLine/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReadLine
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string entrada;
+
+            Console.WriteLine("Escribe tu nombre: ");
+            entrada = Console.ReadLine();
+
+            Console.WriteLine("Hola {0}, ¿Como te encuetras hoy?", entrada);
+
+            entrada = Console.ReadLine();
+
+            Console.WriteLine("Perfecto, que tengas un buen dia. :) ");
+
+            Console.ReadKey();
+        }   
+    }
+}
