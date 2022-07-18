c7117f1e3c9d89dac8832ff9be53a5e6b4d2a5af
claudioalexist <claudiotapia.ctb@gmail.com>
2022-07-18 15:21:29 -0400

    Calculos_conMath

---
+++ Calculos_conMath/Calculos_conMath/Program.cs
@@ -0,0 +1,27 @@
+﻿// Math saca la Raiz cuadrada
+
+
+
+using System;
+namespace Calculos_conMath
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /*double raiz = Math.Sinh(9);
+
+            Console.WriteLine(raiz);
+
+            Console.ReadKey();*/
+
+           double num1 = 534.4; // variable de tipo dobble
+
+            int resultado = (int)(Math.Round(num1)); // metodo row para redonderar 
+
+            Console.WriteLine(resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
