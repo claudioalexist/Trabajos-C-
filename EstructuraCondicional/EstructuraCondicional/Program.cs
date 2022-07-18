f204e224eee9acad164610e3b8e8460ed0a6b69f
claudioalexist <claudiotapia.ctb@gmail.com>
2022-07-18 15:33:33 -0400

    EstructuraCondicional
    EstructuraCondicional con if y else

---
+++ EstructuraCondicional/EstructuraCondicional/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EstructuraCondicional
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num1, num2;
+            int suma;
+            string linea;
+
+            Console.Write("Ingrese primer numero:");
+
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+
+            Console.Write("Ingrese segundo numero:");
+
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+
+            if (num1 >= num2) // ingresamos condicion if -- si num1 es mayor o igual a num2
+            {
+                suma = num1 + num2;
+                Console.WriteLine("la suma es: " + suma);
+            }
+
+            else // En lo contrario muestrame num1 * num2
+            {
+                suma = num1 * num2;
+                Console.WriteLine("El producto es: " + suma);
+
+                suma = num1 / num2;
+                Console.WriteLine("La division es: " + suma);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
